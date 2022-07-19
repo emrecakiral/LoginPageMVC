89f8a1269711c2134c7c1a76ec9739f4bd59a76a
Emre Çakıral <emrecakiral@outlook.com>
2022-07-19 14:51:46 +0300

    Add project files.

---
+++ SahibindenMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GirisSayfaMVC.Controllers
+{
+    using Models;
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(Login model)
+        {
+            bool durum = ModelState.IsValid;
+
+            if (ModelState.IsValid)
+            {
+                
+            }
+            else
+            {
+                ModelState.AddModelError("Hata", "Lütfen belirtilen alanları doldurunuz...");
+            }
+            return View();
+        }
+    }
+}
---
+++ SahibindenMVC/Models/Login.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GirisSayfaMVC.Models
+{
+    public class Login
+    {
+        [EmailAddress(ErrorMessage = "Lütfen geçerli bir E-Posta adresi giriniz.")]
+        [Display(Name ="E-posta")]
+        [Required(ErrorMessage = "E-posta adresinizi giriniz.")]
+        public string Eposta{ get; set; }
+
+
+        [Display(Name ="Şifre")]
+        [Required(ErrorMessage = "Şifrenizi girin.")]
+        public string Password{ get; set; }
+    }
+}
